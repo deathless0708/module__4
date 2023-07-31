9f2f2144cc1c35c1186071bd959c8e79fc8fbc13
deathless0708 <138172790+deathless0708@users.noreply.github.com>
2023-08-01 02:16:17 +0300

    lesson4_test

---
+++ advertisment/app_advertisment/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse as hp
+
+
+# Create your views here.
+def index(request):
+    return hp('Это мой первый сайт!(Хотя получается неа)')
