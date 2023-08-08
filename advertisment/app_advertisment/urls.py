021e12fa047ccaa4b913d8151de8f5f25ab5151d
deathless0708 <138172790+deathless0708@users.noreply.github.com>
2023-08-08 14:55:50 +0300

    lesson_5

--- advertisment/app_advertisment/urls.py
+++ advertisment/app_advertisment/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index #, start
+from .views import index, top_sellers #, start
 
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='main_page'),
+    path('top-sellers/', top_sellers, name='top-sellers')
     # path('start', start)
 ]
--- advertisment/app_advertisment/views.py
+++ advertisment/app_advertisment/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse as hp
 
 
 # Create your views here.
+
 def index(request):
     return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
 
 # def start(request):
 #     return hp('hpfgjhfgjhodphjhhjpijdprthjrtid')
