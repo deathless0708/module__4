9dc75f98d17dde54614f9d5f97e391d1527759b2
deathless0708 <138172790+deathless0708@users.noreply.github.com>
2023-08-30 17:15:43 +0300

    Final

--- 1/main.py
+++ 1/main.py
@@ -1,43 +1,48 @@
+# # def strcounter(string):
+# #     for symbol in string:
+# #         counter = 0
+# #         for syb_symbol in string:
+# #             if symbol == syb_symbol:
+# #                 counter += 1
+# #         print(symbol, counter )
+# #
+# #
+# # strcounter('abcde')
+#
+# # def strcounter(string):
+# #     for symbol in set(string):
+# #         counter = 0
+# #         for syb_symbol in string:
+# #             if symbol == syb_symbol:
+# #                 counter += 1
+# #         print(symbol, counter )
+# #
+# #
+# # strcounter('abcde')
+#
 # def strcounter(string):
+#     syms_counter = {}
 #     for symbol in string:
-#         counter = 0
-#         for syb_symbol in string:
-#             if symbol == syb_symbol:
-#                 counter += 1
-#         print(symbol, counter )
+#         syms_counter[symbol] = syms_counter.get(symbol, 0) + 1
+#     print(syms_counter)
+#
+#     if string == string[::-1]:
+#         print(True)
+#     else:
+#         print(False)
 #
 #
-# strcounter('abcde')
-
-# def strcounter(string):
-#     for symbol in set(string):
-#         counter = 0
-#         for syb_symbol in string:
-#             if symbol == syb_symbol:
-#                 counter += 1
-#         print(symbol, counter )
+# #
 #
 #
-# strcounter('abcde')
-
-def strcounter(string):
-    syms_counter = {}
-    for symbol in string:
-        syms_counter[symbol] = syms_counter.get(symbol, 0) + 1
-    print(syms_counter)
-
-    if string == string[::-1]:
-        print(True)
-    else:
-        print(False)
-
-
+# #31231
+# #
+# # def Asec():
+#     pass
+# strcounter('лепсспел')
 #
 
+def ask_26(a, b):
+    return a>b
 
-#31231
-#
-# def Asec():
-    pass
-strcounter('лепсспел')
-
+print(ask_26(10,16))
